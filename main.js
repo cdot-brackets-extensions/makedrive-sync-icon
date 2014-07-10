ff77c9dba7f25dc7fb1226f49761b5e14898d347
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2014-07-10 15:34:07 -0400

    Initial scaffolding and extension logic

---
+++ main.js
@@ -0,0 +1,51 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+  var SyncUtil = require('./lib/syncutil');
+
+  var $icon;
+
+  ExtensionUtils.loadStyleSheet(module, "./resources/css/style.css");
+
+  $icon = $("<a class=\"icon\">Q</a>")
+  .click(_subMenu)
+  .appendTo($("#status-info .info"));
+
+  function _subMenu() {
+    if (!SyncUtil.is.connected) {
+      // Popup menu logic here
+        //  Options GREYED OUT
+    }
+    if (SyncUtil.is.syncing) {
+      // Popup menu logic here
+        // Sync greyed out
+    }
+    // Popup menu logic here
+      // All options available
+  }
+
+  // Attach listeners
+  SyncUtil.attachListeners({
+    onConnect: function() {
+      // Update UI to show a checkmark
+      console.log("We've connected!");
+    },
+    onDisconnected: function() {
+      // Update UI to show a red 'X'
+      console.log("We've disconnected!");
+    },
+    onError: function(err) {
+      // Update UI to show a warning symbol
+      console.log("Something errored! Here it is: " + err.toString());
+    },
+    onSyncing: function() {
+      // Update UI to show a progress wheel
+      console.log("We started syncing!");
+    },
+    onCompleted: function() {
+      // Update UI to blink slowly, then show a checkmark again
+      console.log("The sync finished!");
+    }
+  });
+});
