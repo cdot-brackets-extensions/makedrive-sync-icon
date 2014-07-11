f74c27e59b1b4ee9fd23091927d98573a0806808
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2014-07-11 11:06:34 -0400

    more

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ define(function(require, exports, module) {
 
 //  ExtensionUtils.loadStyleSheet(module, "./resources/css/style.css");
 
-  $icon = $("<div id=\"status-sync\">Something</div>")
-  .click(_subMenu)
-  .appendTo($("#status-info"));
+  window.on('load', function() {
+    $icon = $("<div id=\"status-sync\">Something</div>")
+    .click(_subMenu)
+    .appendTo($("#status-info"));
+  });
 
   function _subMenu() {
     if (!SyncUtil.is.connected) {
